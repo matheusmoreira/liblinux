6bf2bd40c8485fa2d9559de2c2f28ff3afc8c386
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2018-03-05 00:16:57 -0500

    Add system call function for x86_64

---
+++ source/arch/x86_64/system_call.c
@@ -0,0 +1,18 @@
+long
+system_call_6(long number,
+              long _1, long _2, long _3,
+              long _4, long _5, long _6)
+{
+    long return_value;
+
+    register long r10 __asm__("r10") = _4;
+    register long r8  __asm__("r8")  = _5;
+    register long r9  __asm__("r9")  = _6;
+
+    __asm__ volatile ( "syscall"
+                     : "=a" (return_value)
+                     : "a" (number), "D" (_1), "S" (_2), "d" (_3), "r" (r10), "r" (r8), "r" (r9)
+                     : "rcx", "r11", "cc", "memory");
+
+    return return_value;
+}
