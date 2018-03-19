4f3e4fa22e24f9c2fcf6b9b78495966387a8cc07
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2018-03-18 22:04:20 -0400

    Add const qualifier to write's buffer parameter
    The write system call is declared to operate on a constant buffer.
    Unlike read, it does not write to the buffer supplied by the user.

--- source/system_calls/write.c
+++ source/system_calls/write.c
@@ -1,6 +1,6 @@
 #include <liblinux/system_call.h>
 #include <liblinux/system_calls/write.h>
 
-ssize_t write(unsigned int file_descriptor, char * buffer, size_t count) {
+ssize_t write(unsigned int file_descriptor, const char * buffer, size_t count) {
     return system_call(1, file_descriptor, (long) buffer, count);
 }
