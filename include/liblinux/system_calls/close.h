accec00f7b2951eea2206ffba7f79cc6d582281e
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2018-03-18 22:54:35 -0400

    Add wrapper for the close system call

---
+++ include/liblinux/system_calls/close.h
@@ -0,0 +1,6 @@
+#ifndef LIBLINUX_SYSTEM_CALLS_CLOSE_H
+#define LIBLINUX_SYSTEM_CALLS_CLOSE_H
+
+int close(unsigned int file_descriptor);
+
+#endif /* LIBLINUX_SYSTEM_CALLS_CLOSE_H */
---
+++ source/system_calls/close.c
@@ -0,0 +1,6 @@
+#include <liblinux/system_call.h>
+#include <liblinux/system_calls/close.h>
+
+int close(unsigned int file_descriptor) {
+    return system_call(3, file_descriptor);
+}
