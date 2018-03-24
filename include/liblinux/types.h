67984eda8f2634339779a0b9bc6471600437b8ec
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2018-03-24 17:41:18 -0300

    Use Linux UAPI headers instead of built-in headers

--- include/liblinux/types.h
+++ include/liblinux/types.h
@@ -1,8 +1,9 @@
 #ifndef LIBLINUX_TYPES_H
 #define LIBLINUX_TYPES_H
 
-#include <stddef.h> /* size_t */
+#include <linux/posix_types.h>
 
-typedef long ssize_t;
+typedef __kernel_size_t size_t;
+typedef __kernel_ssize_t ssize_t;
 
 #endif /* LIBLINUX_TYPES_H */
