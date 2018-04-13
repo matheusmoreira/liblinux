cb847c8a40f10586df134b22bd5f15e965ae8cbd
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2018-04-13 18:37:09 -0300

    Improve the memory example
    Introduce a simple memory management data structure.
    Implement deallocation and output functions.

--- examples/memory.c
+++ examples/memory.c
@@ -1,32 +1,57 @@
 #include <liblinux/system_calls/mmap.h>
+#include <liblinux/system_calls/munmap.h>
 #include <liblinux/system_calls/write.h>
-
-#define OUTPUT 1
-#define ERROR 2
 
 #define PRINTABLE_COUNT 95
 #define PRINTABLE_OFFSET 32
 
-static void *allocate(size_t);
+struct memory {
+	size_t length;
+	void *address;
+};
+
+static void allocate(struct memory *, size_t);
+static void deallocate(struct memory *);
+static ssize_t output(struct memory *);
 
 int main(void)
 {
-	char * text = allocate(PRINTABLE_COUNT + 1);
+	struct memory data = {0};
+	char *text = 0;
 	int i = 0;
 
-	text[PRINTABLE_COUNT] = '\n';
-	for (; i < PRINTABLE_COUNT; ++i) {
-		text[i] = PRINTABLE_OFFSET + i;
-	}
+	allocate(&data, PRINTABLE_COUNT + 1);
 
-	if (write(OUTPUT, text, PRINTABLE_COUNT + 1) != (PRINTABLE_COUNT + 1))
+	text = data.address;
+	text[PRINTABLE_COUNT] = '\n';
+	for (; i < PRINTABLE_COUNT; ++i)
+		text[i] = PRINTABLE_OFFSET + i;
+
+	if (output(&data) != data.length)
 		return 1;
+
+	deallocate(&data);
 
 	return 0;
 }
 
-static void *allocate(size_t size)
+static void allocate(struct memory *data, size_t length)
 {
-	return mmap(0, size, PROT_READ | PROT_WRITE,
-	            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	data->length = length;
+	data->address = mmap(0, length, PROT_READ | PROT_WRITE,
+	                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+}
+
+static void deallocate(struct memory *data)
+{
+	munmap(data->address, data->length);
+	data->address = 0;
+	data->length = 0;
+}
+
+#define OUTPUT 1
+
+static ssize_t output(struct memory *data)
+{
+	return write(OUTPUT, data->address, data->length);
 }
