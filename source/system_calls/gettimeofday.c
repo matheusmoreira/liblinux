2773ec35b8a1beb2f44d2c99dab58a076c160f29
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-01-16 03:33:20 -0200

    Fix return value of gettimeofday implementation
    Return value was changed to int in the header only.
    This generated a multiple definitions error, breaking the build process.

--- source/system_calls/gettimeofday.c
+++ source/system_calls/gettimeofday.c
@@ -2,7 +2,7 @@
 #include <liblinux/system_calls/gettimeofday.h>
 #include <liblinux/definitions.h>
 
-long gettimeofday(struct timeval *tv, struct timezone *tz)
+int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
 	return system_call(__NR_gettimeofday, tv, tz);
 }
