0b83ffd857dc1b4eb703dc0aae609bc1d29ef198
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-05-06 17:16:26 -0300

    Add r8, r9 and r10 to the list of outputs
    On Linux x86_64, system calls clobbers rcx, r8, r9, r10 and r11.
    However, r8, r9 and r10 are inputs to the syscall instruction.
    The GCC manual states:
    
    > When the compiler selects the registers to use
    > to represent the input operands,
    > it does not use any of the clobbered registers.
    
    So they can't be placed in the list of clobbered registers.
    Placing them in the list of outputs achieves the same result,
    even though they are not true outputs.
    
    References:
    
    https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#Input-Operands
    https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/tools/include/nolibc/nolibc.h

--- source/arch/x86_64/system_call.c
+++ source/arch/x86_64/system_call.c
@@ -11,10 +11,14 @@ system_call(long number, long _1, long _2, long _3, long _4, long _5, long _6)
 	register long r8  __asm__("r8")  = _5;
 	register long r9  __asm__("r9")  = _6;
 
+	/* r8, r9 and r10 may be clobbered but can't be in the clobbers list
+	   because the compiler won't use clobbered registers as inputs.
+	   So they're placed in the outputs list instead. */
 	__asm__ volatile
 	("syscall"
 
-		: "=a" (return_value)
+		: "=a" (return_value),
+		  "=r" (r8), "=r" (r9), "=r" (r10)
 		: "r" (rax),
 		  "r" (rdi), "r" (rsi), "r" (rdx),
 		  "r" (r10), "r" (r8),  "r" (r9)
