f7212d0b388269a740fd75d75a79b528f0e0ca0b
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-05-06 16:56:17 -0300

    Specify all system call ABI registers explicitly
    This is easier to understand compared to architecture-specific
    register allocation constraints.
    The previous code used "D", "S" and "d" constraints
    for the di (rdi), si (rsi) and d (rdx) x86_64 registers.
    
    References:
    
    https://gcc.gnu.org/onlinedocs/gcc/Simple-Constraints.html
    https://gcc.gnu.org/onlinedocs/gcc/Machine-Constraints.html

--- source/arch/x86_64/system_call.c
+++ source/arch/x86_64/system_call.c
@@ -3,6 +3,10 @@ system_call(long number, long _1, long _2, long _3, long _4, long _5, long _6)
 {
 	long return_value;
 
+	register long rax __asm__("rax") = number;
+	register long rdi __asm__("rdi") = _1;
+	register long rsi __asm__("rsi") = _2;
+	register long rdx __asm__("rdx") = _3;
 	register long r10 __asm__("r10") = _4;
 	register long r8  __asm__("r8")  = _5;
 	register long r9  __asm__("r9")  = _6;
@@ -11,9 +15,9 @@ system_call(long number, long _1, long _2, long _3, long _4, long _5, long _6)
 	("syscall"
 
 		: "=a" (return_value)
-		: "a" (number),
-		  "D" (_1), "S" (_2), "d" (_3),
-		  "r" (r10), "r" (r8), "r" (r9)
+		: "r" (rax),
+		  "r" (rdi), "r" (rsi), "r" (rdx),
+		  "r" (r10), "r" (r8),  "r" (r9)
 		: "rcx", "r11", "cc", "memory");
 
 	return return_value;
