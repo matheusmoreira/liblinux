b460f00d662cac683ddf8de2f158d93ab15f049b
Matheus Afonso Martins Moreira <matheus.a.m.moreira@gmail.com>
2019-05-07 19:27:47 -0300

    Use the "+" prefix for the rax register constraint
    The rax register is simultaneously an input and output register
    since it Linux reads the system call number from it
    and later writes the return value to it.
    The "+" prefix was designed for these situations:
    
    > Operands using the ‘+’ constraint modifier
    > count as two operands (that is, both as input and output)
    
    This allows rax to be listed only once, avoiding needless repetition.
    The list of inputs now contains just the six system call parameters.
    All operand register constraints are now generic
    and only the constraints applied to the variables they reference matter.
    The return_value variable is not needed anymore and can be removed.
    
    References:
    
    https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html#OutputOperands

--- source/arch/x86_64/system_call.c
+++ source/arch/x86_64/system_call.c
@@ -1,8 +1,6 @@
 long
 system_call(long number, long _1, long _2, long _3, long _4, long _5, long _6)
 {
-	long return_value;
-
 	register long rax __asm__("rax") = number;
 	register long rdi __asm__("rdi") = _1;
 	register long rsi __asm__("rsi") = _2;
@@ -17,12 +15,11 @@ system_call(long number, long _1, long _2, long _3, long _4, long _5, long _6)
 	__asm__ volatile
 	("syscall"
 
-		: "=a" (return_value),
+		: "+r" (rax),
 		  "=r" (r8), "=r" (r9), "=r" (r10)
-		: "r" (rax),
-		  "r" (rdi), "r" (rsi), "r" (rdx),
+		: "r" (rdi), "r" (rsi), "r" (rdx),
 		  "r" (r10), "r" (r8),  "r" (r9)
 		: "rcx", "r11", "cc", "memory");
 
-	return return_value;
+	return rax;
 }
